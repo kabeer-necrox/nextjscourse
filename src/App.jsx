1f05441d40d3118b91822e7ac7c39cfe2476908c
kabeer-necrox <khushkabeer@gmail.com>
2024-01-16 11:52:23 +0300

    thisis the meassage of

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>this is the kabeer react js git hub page</h1>
+      <p>this is github page</p>
+    </>
+  )
+}
+
+export default App
