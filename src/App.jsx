a606a05dfd4be1500b1395144bcff18571953fe7
kabeer-necrox <khushkabeer@gmail.com>
2024-01-20 18:49:34 +0300

    here is net

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+const dec=()=>{
+  setCount(count -1)
+
+}
+
+const inc=()=>{
+  
+  setCount(count +1)
+  
+}
   return (
     <>
-    <h1>this is the kabeer react js git hub page</h1>
-      <p>this is github page</p>
+  <h1>this is the counter app</h1>
+
+  <h1> {count}</h1>
+
+  <button onClick={inc}>increment</button>
+  <button onClick={dec}>decrement</button>
+     
     </>
   )
 }
